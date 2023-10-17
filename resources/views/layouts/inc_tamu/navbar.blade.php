b153384fb120ddbf868e0adbc5b28a1d6711afda
Rinagustisya <rinaagustisya885@gmail.com>
2023-10-17 22:52:01 +0700

    feat : tamu or guest page

---
+++ resources/views/layouts/inc_tamu/navbar.blade.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-info shadow">
+    <div class="container">
+        <a class="navbar-brand h1" href="index.html">
+            <img src="images/logo.JPEG" width="30" height="30" class="d-inline-block align-top img-circle" alt="Logo">
+            Hotel Rina
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.html">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="kamar.html">Kamar</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.html">Fasilitas</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="about.html">About</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/layouts/tamu.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>{{ config('app.name') }}</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="/adminlte/plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="/adminlte/dist/css/adminlte.min.css">
+
+  <link rel="stylesheet" href="/css/style.css">
+</head>
+<body class="hold-transition">
+<!-- As a link -->
+<!-- navbar -->
+@include('layouts.inc_tamu.navbar')
+<!-- end navbar -->
+
+<div class="container-fluid p-0">
+    <img src="images/banner.jpg" class="img img-fluid w-100">
+</div>
+
+<div class="container content">
+    @yield('content')
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <span class="text-muted"><strong>Copyright &copy; 2023 Rina Agustisya.</strong> All rights reserved.</span>
+    </div>
+</footer>
+
+<!-- jQuery -->
+<script src="/adminlte/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="/adminlte/dist/js/adminlte.min.js"></script>
+</body>
+</html>
