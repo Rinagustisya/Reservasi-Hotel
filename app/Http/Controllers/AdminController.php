cfe9ceb4930d23ed707fe51a91535949cd9c8402
MyName269 <rinaagustisya885@gmail.com>
2023-10-25 15:04:30 +0700

    feat : method destroy & status destroy

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -119,8 +119,10 @@ class AdminController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Admin $admin)
     {
-        //
+        $admin->delete();
+
+        return redirect()->route('admin.index')->with('status', 'destroy');
     }
 }
--- resources/views/components/status.blade.php
+++ resources/views/components/status.blade.php
@@ -15,3 +15,12 @@
   </button>
 </div>
 @endif
+
+@if ( session('status') == 'destroy')
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+  <strong>Berhasil Hapus!</strong> Data Telah dihapus
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+@endif
